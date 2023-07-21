4a3ef4a443231c5b32dafa7b8f66663ec53a60e1
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-07-21 13:29:46 +1200

    added some include statements

---
+++ content/footer.php
@@ -0,0 +1,3 @@
+<div class="box footer">
+CC Harish Peddi 2023
+</div>    <!-- / footer -->
---
+++ content/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Website / database featuring famous quotes">
+    <meta name="keywords" content="quotations, quotes, famous">
+    <meta name="author" content="Harish Peddi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Quick Quotes</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/l3_data_style.css"> 
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+</head>
