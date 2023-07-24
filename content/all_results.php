8d0d253f8d5141db289d879377db1f0da320b3c9
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-07-25 11:04:33 +1200

    shows all quotes

--- content/all_results.php
+++ content/all_results.php
@@ -1,1 +1,41 @@
-<h2>All Results</h2>
+<?php
+
+$find_sql = "
+SELECT
+
+q.*,
+a.*,
+s1.Subject AS Subject1,
+s2.Subject AS Subject2,
+s3.Subject AS Subject3
+
+FROM Quotes q
+
+JOIN author a ON a.Author_ID = q.Author_ID
+
+";
+$find_query = mysqli_query($dbconnect, $find_sql);
+$find_rs = mysqli_fetch_assoc($find_query);
+$find_count = mysqli_num_rows($find_query);
+
+?>
+
+<h2>All Results (<?php echo $find_count ?>)</h2>
+
+<?php 
+
+while($find_rs = mysqli_fetch_assoc($find_query)) {
+
+    $quote = $find_rs['Quote'];
+
+    ?>
+
+    <div class="results">
+        <?php echo $quote ?>
+    </div>
+    <br/>
+
+    <?php
+}
+
+?>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     include("functions.php");
 
     // connect to database
-    $dbconnect=mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME)
+    $dbconnect=mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if(mysqli_connect_errno()) {
         echo "Connection failed:".mysqli_connect_error();
