bb1186811ea645982a76a8012e315c3d02f61764
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2023-07-26 10:05:41 +1200

    Update all_results.php

--- content/all_results.php
+++ content/all_results.php
@@ -1,7 +1,6 @@
 <?php
 
-$find_sql = "
-SELECT
+$find_sql = "SELECT
 
 q.*,
 a.*,
@@ -9,9 +8,13 @@ s1.Subject AS Subject1,
 s2.Subject AS Subject2,
 s3.Subject AS Subject3
 
-FROM Quotes q
+FROM
+quotes q
 
 JOIN author a ON a.Author_ID = q.Author_ID
+JOIN all_subjects s1 ON q.Subject1_ID = s1.SubjectID
+JOIN all_subjects s2 ON q.Subject2_ID = s2.SubjectID
+JOIN all_subjects s3 ON q.Subject3_ID = s3.SubjectID
 
 ";
 $find_query = mysqli_query($dbconnect, $find_sql);
@@ -27,11 +30,42 @@ $find_count = mysqli_num_rows($find_query);
 while($find_rs = mysqli_fetch_assoc($find_query)) {
 
     $quote = $find_rs['Quote'];
+    
+    $author_first = $find_rs['First'];
+    $author_middle = $find_rs['Middle'];
+    $author_last = $find_rs['Last'];
+
+    // put space before middle initial so if it's blank we don't have
+    // spacing issues
+    $author_middle = " ".$author_middle;
+
+    // set up subjects
+    $subject_1 = $find_rs['Subject1'];
+    $subject_2 = $find_rs['Subject2'];
+    $subject_3 = $find_rs['Subject3'];
+
+    // put subjects in list so that we can iterate through it
+    $all_subjects = array($subject_1, $subject_2, $subject_3);
 
     ?>
 
     <div class="results">
         <?php echo $quote ?>
+
+        <p><i><?php echo $author_first.$author_middle." ".$author_last ?></i></p>
+
+        <?php 
+        
+        // iterate through all_subjects list and output subject if it is not blank
+        foreach($all_subjects as $subject) {
+            // check if subject is n/a
+            if($subject != "n/a") {
+                echo "<span class='tag'>$subject</span>&nbsp;&nbsp;";
+            }
+
+        }
+
+        ?>
     </div>
     <br/>
 
